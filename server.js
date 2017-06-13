755691b6d80dd798dcbe243833cf9696847b5732
viragmali <viragmali88@gmail.com>
2017-06-13 07:50:06 +0000

    Initial commit

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
+app.use(express.static('public'));
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+app.get('/meows',function(req,res,next){
+  Meow.find({},function(err,meows){
+    return res.json(meows);
+  });
+});
+app.post('/meows', function(req, res, next){	
+		var newMeow = new Meow({
+			text: req.body.newMeow		
+		});
+		newMeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+	});
+app.put('/meows/remove', function(req, res, next){	
+var meowId=req.body.meow._id;		
+		Meow.remove({_id: meowId}, function(err){
+			return res.send("Deleted Successfully");
+	});
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
